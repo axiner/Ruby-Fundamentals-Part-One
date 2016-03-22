9fb40cb55cce22ffbc60db8b26cd55f9bbd52295
axiner <axiner.w@gmail.com>
2016-03-22 13:57:26 -0400

    exercise2.rb

---
+++ exercise2.rb
@@ -0,0 +1,10 @@
+puts 55 * 0.13
+
+puts "The good tip for a 55 dollar meal is #{55*0.13}"
+
+integer = 1
+puts "The integer is #{integer}."
+
+puts "The result is #{45628*7839}"
+
+puts (10 < 20 && 30 < 20)||!(10 == 11)
